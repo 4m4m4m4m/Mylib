44e995b1e8220275d2daff531fc49c2c105cb940
4m4m4m4m <sorokinkirillsorokin2005@gmail.com>
2025-07-15 10:17:31 +0300

    connected db and made model for books

--- main.py
+++ main.py
@@ -1,46 +1,63 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 import uvicorn
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import select
 from pydantic import BaseModel, Field
+from datetime import date
+from typing import Annotated
 
 app = FastAPI()
+engine = create_async_engine('sqlite+aiosqlite:///books.db')
+new_session = async_sessionmaker(engine, expire_on_commit=False)
 
-books = [
-    {
-        "id":1,
-        "title":"async in python",
-        "autor":"Some dude"
-    },
-    {
-        "id":2,
-        "title":"async in python 2",
-        "autor":"Same dude"
-    },
-]
+async def get_session():
+    async with new_session() as session:
+        yield session
 
-@app.get("/books")
-def read_books():
-    return books
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
 
-@app.get("/books/{book_id}")
-def read_book(book_id:int):
-    for book in books:
-        if book["id"] == book_id:
-            return book
-    raise HTTPException(status_code=404, detail="Book not found")
+class Base(DeclarativeBase):
+    pass
+
+class BookModel(Base):
+    __tablename__ = "books"
+
+    id:Mapped[int] = mapped_column(primary_key=True)
+    title:Mapped[str]
+    author:Mapped[str]
+    date:Mapped[date]
+
+@app.get("/setupDB")
+async def SetupDB():
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.drop_all)
+        await connection.run_sync(Base.metadata.create_all)
 
 class NewBook(BaseModel):
     title:str
-    author:str = Field()
+    author:str
+    date:date
+
+class BookSchema(NewBook):
+    id:int
 
 @app.post('/books')
-def create_book(new_book: NewBook):
-    books.append({
-        "id":len(books)+1,
-        "title": new_book.title,
-        "author": new_book.author,
-    })
-    return {"success": True, "message":"Book "+new_book.title+" added"}
+async def create_book(data: NewBook, session: SessionDep):
+    new_book = BookModel(
+        title=data.title,
+        author=data.author,
+        date=data.date,
+    )
+    session.add(new_book)
+    await session.commit()
+    return {"success": True, "message": "Book "+data.title+" added"}
 
+@app.get('/books')
+async def get_books(session: SessionDep):
+    query = select(BookModel)
+    result = await session.execute(query)
+    return result.scalars().all()
 
 if __name__ == "__main__": 
     uvicorn.run("main:app", reload=True)
