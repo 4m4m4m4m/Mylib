d638c4ad944319b852a85b7c99ad3a4e5c3cd0ee
4m4m4m4m <sorokinkirillsorokin2005@gmail.com>
2025-07-22 14:50:25 +0300

    created search and started using form

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException, Depends, Request
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, Form, HTTPException, Depends, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -10,17 +11,25 @@ from sqlalchemy import select
 from pydantic import BaseModel, Field
 from datetime import date
 from typing import Annotated
-from models.mainModels import BookModel, Base
-import os
+from models.mainModels import *
 from pathlib import Path
+from authx import AuthX, AuthXConfig, TokenPayload
 
-# print(f"Current working directory: {os.getcwd()}")
-# print(f"Template directory exists: {os.path.exists('src/templates')}")
-# print(f"Index.html exists: {os.path.exists('src/templates/index.html')}")
-
-app = FastAPI()
 engine = create_async_engine('sqlite+aiosqlite:///src/books.db')
 new_session = async_sessionmaker(engine, expire_on_commit=False, autocommit=False)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI): #Startup/shutdown code
+    # Startup code
+    async with engine.begin() as conn:
+        # await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    print("Database initialized")
+    yield
+    # Shutdown code would go here
+    print("Application shutting down")
+
+app = FastAPI(lifespan=lifespan)
 
 templates = Jinja2Templates(directory="src/templates") #Обьявляется путь к templates 
 
@@ -50,11 +59,16 @@ class BookResponse(BaseModel):
     class Config:
         from_attributes = True
 
-@app.get('/SetupDB')
-async def SetupDB():
-    async with engine.begin() as connection:
-        await connection.run_sync(Base.metadata.drop_all)
-        await connection.run_sync(Base.metadata.create_all)
+class UserLoginModel(BaseModel):
+    username:str
+    password:str
+
+config = AuthXConfig()
+config.JWT_SECRET_KEY = "mysecretkey"
+config.JWT_ACCESS_COOKIE_NAME = "accesstoken"
+config.JWT_TOKEN_LOCATION = ["cookies"]
+
+security = AuthX(config=config)
 
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request, session: SessionDep):
@@ -63,7 +77,43 @@ async def index(request: Request, session: SessionDep):
     books = [BookResponse.model_validate(book) for book in result.scalars().all()]
     return templates.TemplateResponse("index.html", {"request": request, "result":books})
 
-@app.post('/books')
+@app.post('/', response_class=HTMLResponse)
+async def index_post(request: Request, session: SessionDep, search: Annotated[str,Form()]):
+    if not search:
+        query = select(BookModel)
+    else:
+        query = select(BookModel).filter(BookModel.title.contains(search))
+    result = await session.execute(query)
+    books = [BookResponse.model_validate(book) for book in result.scalars().all()]
+    return templates.TemplateResponse("index.html", {"request": request, "result":books})
+
+@app.post('/register')
+async def register(session:SessionDep, data:UserLoginModel):
+    new_user = UserModel(
+        name = data.username,
+        password = data.password
+    )
+    session.add(new_user)
+    await session.commit()
+    return {"success":True, "message":"User "+data.username+" added"}
+
+@app.post('/login')
+async def login(responce:Response ,session:SessionDep, cred:UserLoginModel):
+    user_result = await session.execute(select(UserModel).filter_by(name=cred.username))
+    user = user_result.scalars().first()
+
+    if user is None:
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
+    
+    if user.password == cred.password:
+        token = security.create_access_token(uid=str(user.id))
+        responce.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
+        return {"access_token": token}
+    else:
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
+
+
+@app.post('/books', dependencies=[Depends(security.access_token_required)])
 async def create_book(data: NewBook, session: SessionDep):
     new_book = BookModel(
         title=data.title,
--- src/models/mainModels.py
+++ src/models/mainModels.py
@@ -11,3 +11,10 @@ class BookModel(Base):
     title:Mapped[str]
     author:Mapped[str]
     date:Mapped[date]
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str] = mapped_column(unique=True)
+    password:Mapped[str]
